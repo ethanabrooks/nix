bc2a7843704dd2c26ed727ff63acea9e2131895b
Ethan Brooks <ethanabrooks@gmail.com>
2022-07-11 23:06:19 -0400

    add stuff to home.nix

--- home.nix
+++ home.nix
@@ -3,47 +3,96 @@
 {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = "ethanbro";
-  home.homeDirectory = "/home/ethanbro";
+  home = {
+    # Raw configuration files
+    file.".vimrc".source = ./vimrc;
 
-  # Packages that should be installed to the user profile.
-  home.packages = [
-    pkgs.htop
-    pkgs.tmux
-    pkgs.vim
-    pkgs.neovim
-    pkgs.ncdu
-    pkgs.poetry
-  ];
+    homeDirectory = "/home/ethanbro";
 
-  # Raw configuration files
-  home.file.".vimrc".source = ./vimrc;
+    # Packages that should be installed to the user profile.
+    packages = with pkgs; [
+      htop
+      fzf
+      mosh
+      ncdu
+      nix
+      nixpkgs-fmt
+      poetry
+      pure-prompt
+      ripgrep
+      tmux
+      tree
+      zsh
+    ];
 
-  programs.git = {
-    enable = true;
-    userName = "ethanabrooks";
-    userEmail = "ethanabrooks@gmail.com";
-    aliases = {
+    sessionVariables.EDITOR = "nvim";
+
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "22.05";
+
+    username = "ethanbro";
+  };
+
+  programs = {
+    git = {
+      enable = true;
+      userName = "ethanabrooks";
+      userEmail = "ethanabrooks@gmail.com";
+      aliases = {
         lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
         br = "branch";
         co = "checkout";
         cm = "commit -am";
         df = "!git reset && git diff";
+      };
+    };
+
+    # Let Home Manager install and manage itself.
+    home-manager = {
+      enable = true;
+    };
+
+    neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+      extraConfig = builtins.readFile ./init.vim;
+      plugins = with pkgs.vimPlugins; [
+        ale
+        gruvbox
+        nerdcommenter
+        vim-nix
+      ];
+    };
+
+    zsh = {
+      enable = true;
+      enableSyntaxHighlighting = true;
+      history.size = 10000;
+      #oh-my-zsh.enable = true; # sane defaults
+      prezto = {
+        enable = true;
+        prompt.theme = "pure";
+      };
+      shellAliases = {
+        ll = "ls -l";
+        update = "home-manager switch -b backup";
+      };
+      zplug = {
+        enable = true;
+        plugins = [
+          { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
+        ];
+      };
     };
   };
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "22.05";
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 
   services.gpg-agent = {
     enable = true;
