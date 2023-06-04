922d13ed90cf4f265d7717e0d5ca7c5b7cc6a5c1
ethanabrooks <ethanabrooks@gmail.com>
2023-06-04 02:17:06 -0400

    delete commented-out code

--- home-manager/cmp.lua
+++ home-manager/cmp.lua
@@ -28,11 +28,6 @@ local config = function()
 		TypeParameter = "󰅲",
 	}
 	cmp.setup({
-		--    snippet = {
-		--      expand = function(args)
-		--        require("luasnip").lsp_expand(args.body)
-		--      end,
-		--    },
 		mapping = {
 			["<C-j>"] = cmp.mapping(
 				cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
@@ -56,9 +51,6 @@ local config = function()
 		formatting = {
 			format = function(entry, vim_item)
 				vim_item.kind = kind_icons[vim_item.kind]
-				-- Uncomment to see which sources provide each completion (useful for debugging)
-				-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
-				-- vim_item.menu = entry.source.name
 				return vim_item
 			end
 		},
@@ -68,23 +60,6 @@ local config = function()
 		sources = {
 			{ name = "nvim_lsp" },
 			{ name = "buffer" },
-			--      { name = "nvim_lsp_signature_help" },
-			--      {
-			--        name = "treesitter",
-			--        entry_filter = function(entry)
-			--          local kind = require('cmp.types').lsp.CompletionItemKind[entry:get_kind()]
-			--          for _, v in ipairs({ 'Text', 'Comment' }) do
-			--            if v == kind then
-			--              return false
-			--            end
-			--          end
-			--          return true
-			--        end
-			--      },
-			--      { name = "path" },
-			--      { name = "nvim_lua" },
-			--      { name = "luasnip" },
-			--      { name = "spell" },
 		},
 		window = {
 			completion = cmp.config.window.bordered({
@@ -95,46 +70,6 @@ local config = function()
 			}),
 		},
 	})
-
-	--  -- Completion for search and command line
-	--  cmp.setup.cmdline({ "/", "?" }, {
-	--    mapping = cmp.mapping.preset.cmdline(),
-	--    sources = {
-	--      { name = "buffer" },
-	--    },
-	--  })
-	--  cmp.setup.cmdline(":", {
-	--    mapping = cmp.mapping.preset.cmdline(),
-	--    sources = cmp.config.sources({
-	--      { name = "path" },
-	--    }, {
-	--      { name = "cmdline" },
-	--    }),
-	--  })
-
-	--  -- Separate settings for writing
-
-	--  local writing_sources = {
-	--    { name = "luasnip" },
-	--    { name = "buffer" },
-	--    { name = "spell" },
-	--    { name = "emoji" },
-	--    { name = "latex_symbols" },
-	--  }
-	--
-	--  local writing_filetypes = {
-	--    "markdown",
-	--    "org",
-	--    "neorg",
-	--    "latex",
-	--    "html",
-	--  }
-	--
-	--  for _, t in pairs(writing_filetypes) do
-	--    cmp.setup.filetype(t, {
-	--      sources = cmp.config.sources(writing_sources),
-	--    })
-	--  end
 end
 
 config()
