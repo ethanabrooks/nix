51a805a2d286dceaba4e43f39e15f3531192bdcd
ethanabrooks <ethanabrooks@gmail.com>
2023-06-04 11:26:35 -0400

    inline setup commands for mini-nvim.lua

--- home-manager/mini-nvim.lua
+++ home-manager/mini-nvim.lua
@@ -1,13 +1,11 @@
-local config = function()
-	-- Enhanced textobjects and pairs
-	require("mini.ai").setup()
-	require("mini.animate").setup()
-	-- require("mini.basics").setup()
-	require("mini.comment").setup()
-	-- Move selected text around
-	require("mini.move").setup()
-	require("mini.pairs").setup()
-	require("mini.surround").setup()
-end
-
-config()
+-- Enhanced textobjects and pairs
+require("mini.ai").setup()
+require("mini.animate").setup()
+require("mini.comment").setup()
+require("mini.cursorword").setup()
+require("mini.hipatterns").setup()
+-- Move selected text around
+require("mini.move").setup()
+require("mini.pairs").setup()
+require("mini.statusline").setup()
+require("mini.surround").setup()
