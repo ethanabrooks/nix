3d56f0af2b66078e8941ad018f969ab0705f1fff
ethanabrooks <ethanabrooks@gmail.com>
2023-12-20 09:58:32 -0500

    nest all of the config keys in wezterm.lua and add ssh_domains

--- home-manager/wezterm.lua
+++ home-manager/wezterm.lua
@@ -13,37 +13,50 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Oceanic-Next'
-config.font_size = 20
-config.keys = {{
-    key = 'Enter',
-    mods = 'CMD',
-    action = wezterm.action.SplitHorizontal {
-        domain = 'CurrentPaneDomain'
-    }
-}, {
-    key = 'Enter',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.SplitVertical {
-        domain = 'CurrentPaneDomain'
-    }
-}, {
-    key = 'h',
-    mods = 'CMD',
-    action = wezterm.action.ActivatePaneDirection "Left"
-}, {
-    key = 'j',
-    mods = 'CMD',
-    action = wezterm.action.ActivatePaneDirection "Down"
-}, {
-    key = 'k',
-    mods = 'CMD',
-    action = wezterm.action.ActivatePaneDirection "Up"
-}, {
-    key = 'l',
-    mods = 'CMD',
-    action = wezterm.action.ActivatePaneDirection "Right"
-}}
+config = {
+    color_scheme = 'Oceanic-Next',
+    font_size = 20,
+    keys = {{
+        key = 'Enter',
+        mods = 'CMD',
+        action = wezterm.action.SplitHorizontal {
+            domain = 'CurrentPaneDomain'
+        }
+    }, {
+        key = 'Enter',
+        mods = 'CMD|SHIFT',
+        action = wezterm.action.SplitVertical {
+            domain = 'CurrentPaneDomain'
+        }
+    }, {
+        key = 'h',
+        mods = 'CMD',
+        action = wezterm.action.ActivatePaneDirection "Left"
+    }, {
+        key = 'j',
+        mods = 'CMD',
+        action = wezterm.action.ActivatePaneDirection "Down"
+    }, {
+        key = 'k',
+        mods = 'CMD',
+        action = wezterm.action.ActivatePaneDirection "Up"
+    }, {
+        key = 'l',
+        mods = 'CMD',
+        action = wezterm.action.ActivatePaneDirection "Right"
+    }},
+    ssh_domains = {{
+        -- This name identifies the domain
+        name = 'rldl17',
+        -- The hostname or address to connect to. Will be used to match settings
+        -- from your ssh config file
+        remote_address = 'rldl17.eecs.umich.edu',
+        -- The username to use on the remote host
+        username = 'ethanbro',
+        remote_wezterm_path = '/home/ethanbro/.nix-profile/bin/wezterm'
+    }}
+
+}
 
 -- and finally, return the configuration to wezterm
 return config
