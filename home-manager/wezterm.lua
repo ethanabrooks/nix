db8103276949a10c1a92b7b887498b7a5cd33d8d
ethanabrooks <ethanabrooks@gmail.com>
2023-12-19 23:10:56 -0500

    update wezterm.lua

--- home-manager/wezterm.lua
+++ home-manager/wezterm.lua
@@ -7,13 +7,43 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'OceanicNext'
+config.color_scheme = 'Oceanic-Next'
+config.font_size = 20
+config.keys = {{
+    key = 'Enter',
+    mods = 'CMD',
+    action = wezterm.action.SplitHorizontal {
+        domain = 'CurrentPaneDomain'
+    }
+}, {
+    key = 'Enter',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.SplitVertical {
+        domain = 'CurrentPaneDomain'
+    }
+}, {
+    key = 'h',
+    mods = 'CMD',
+    action = wezterm.action.ActivatePaneDirection "Left"
+}, {
+    key = 'j',
+    mods = 'CMD',
+    action = wezterm.action.ActivatePaneDirection "Down"
+}, {
+    key = 'k',
+    mods = 'CMD',
+    action = wezterm.action.ActivatePaneDirection "Up"
+}, {
+    key = 'l',
+    mods = 'CMD',
+    action = wezterm.action.ActivatePaneDirection "Right"
+}}
 
 -- and finally, return the configuration to wezterm
 return config
